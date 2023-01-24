0d70dc6bf16c68feb7489842c9c0309c32a2f0c2
Raju Chougule <rjchgl08@gmail.com>
2023-01-24 16:57:59 +0530

    delete book from ui

--- appes5.js
+++ appes5.js
@@ -43,6 +43,13 @@ UI.prototype.showAlert = function (message, className) {
   }, 3000);
 };
 
+// Delete Book
+UI.prototype.deleteBook = function (target) {
+  if (target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+};
+
 // Clear Fields
 UI.prototype.clearFields = function () {
   document.getElementById('title').value = '';
@@ -50,7 +57,7 @@ UI.prototype.clearFields = function () {
   document.getElementById('isbn').value = '';
 };
 
-// Event listeners
+// Event listener to add book
 document.getElementById('book-form').addEventListener('submit', function (e) {
   // Get form values
   const title = document.getElementById('title').value,
@@ -76,6 +83,20 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
     // Clear fields
     ui.clearFields();
   }
+
+  e.preventDefault();
+});
+
+// Event listener to delete
+document.getElementById('book-list').addEventListener('click', function (e) {
+  // Instantiate UI
+  const ui = new UI();
+
+  // Delete Book
+  ui.deleteBook(e.target);
+
+  // Show message
+  ui.showAlert('Book Removed!', 'success');
 
   e.preventDefault();
 });
